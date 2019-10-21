b54601597903b10035bd5272b415e563bcc985d7
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-21 13:01:28 -0700

    Add hello world solution

--- javascript/challenges/01-hello-world.js
+++ javascript/challenges/01-hello-world.js
@@ -2,7 +2,7 @@ const sleep = require("../helpers/sleep");
 
 // output should be displayed after the timer
 
-const func = (() => {
-  sleep(5000);
-  console.log("hello");
+const func = (async () => {
+  await sleep(5000);
+  return console.log("hello");
 })();
