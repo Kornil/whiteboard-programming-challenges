6e83f4fc32c46a1c8867905ce9c9060f09541e41
ir3ne <irene.tomaini@gmail.com>
2019-11-12 10:49:26 +0100

    update solution 06

--- javascript/solutions/06-unreachable-condition-solution.js
+++ javascript/solutions/06-unreachable-condition-solution.js
@@ -4,4 +4,4 @@
 // func(0,1) => "world"
 // func(1,2) => "world"
 
-const func = (num1, num2) => (Boolean(num1 && num2) === false ? "hello" : "world");
+const func = (num1, num2) => (num1 && num2 ? "hello" : "world");
